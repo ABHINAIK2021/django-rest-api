bafc7d59fe0dd68e191ead9ac95743601ee80ecb
ABHINAIK2021 <abhishek.naik24082000@gmail.com>
2022-03-22 22:29:38 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://abhisheknaik-abhinaik2021.vercel.app',
+    'http://abhisheknaik-git-main-abhinaik2021.vercel.app',
+    'http://abhisheknaik.vercel.app',
+]
